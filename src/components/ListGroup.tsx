12c70326fdf6ce8c24e617c499cde25efb2c3cb2
Rodrigo Matias De Souza <Rodrigo.MatiasDeSouza@coxautoinc.com>
2023-10-30 19:55:42 +1100

    Conditional Changes
    Conditional change added to the component in order to render or not the element.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["new york", "Adelaide", "Tokyo", "Longon", "Paris"];
+  let items = ["new york", "Adelaide", "Tokyo", "Longon", "Paris"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
